f004e66e591e71736c580a1bfe488c376fa2cd3a
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2023-06-08 13:49:47 -0400

    add update.

---
+++ src/helpers.js
@@ -0,0 +1,14 @@
+function readJSONFile(path, fileName) {
+    const collection = readFilesSync(`${path}/${fileName}`, "utf8");
+    return collection ? JSON.parse(collection) : [];
+}
+
+function writeJSONFile(path, fileName, data) {
+    data = JSON.stringify(data);
+    return writeFileSync(`${path}/${fileName}`, data, { encoding: "utf-8" });
+}
+
+module.exports = {
+    readJSONFile,
+    writeJSONFile,
+};
