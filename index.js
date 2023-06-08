c0ea59acee4db9d8ca3c3abdbafac3d20d2a42aa
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2023-06-08 15:50:31 -0400

    add to index.js.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log("Welcome to my app!");
+const { readJSONFile, writeJSONFile } = require("./src/helpers");
+const {
+    create,
+    index,
+    show,
+    destroy,
+    update,
+    inventory
+} = require("./src/inventoryController")
+
+
+const run = () => {
+
+    const action = process.argv[2];
+
+    const item = process.argv[3];
+    
+    let items = readJSONFile("./data", "inventory.json")
+
+    let writeToFile = false;
+    let updatedItems = [];
+
+    switch (action) {
+        case "index":
+            const allItems = index(items)
+            console.log(allItems);
+            break;
+        case "create":
+            console.log("New Item Created")
+            updatedItems = create(items, item)
+            writeToFile = true;
+            break;
+        case "show":
+            const foundItem = show(items, item)
+            console.log(foundItem)
+            break;
+        
+    }
+}
